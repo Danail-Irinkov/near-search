41707d259ea688484c752811abddd980e8e2bbeb
Maix <darkmitko@abv.bg>
2022-02-14 19:28:47 +0200

    add Contract calling contracts

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,7 +1,7 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::json_types::Base64VecU8;
+// use near_sdk::near_bindgen;
 use near_sdk::{env, near_bindgen, AccountId};
-
-near_sdk::setup_alloc!();
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
@@ -9,8 +9,8 @@ pub struct NearSearch;
 
 #[near_bindgen]
 impl NearSearch {
-    #[payable]
-	pub fn call_contract(&mut self, account_id: AccountId, method_name: Vec<u8>, arguments: Vec<u8>) {
+    // #[payable]
+	pub fn call_contract(&mut self, account_id: AccountId, method_name: Base64VecU8, arguments: Base64VecU8) {
         // Todo: if there is an error should return the deposit back to the sender
         
         if env::predecessor_account_id() == env::current_account_id() {
@@ -20,11 +20,7 @@ impl NearSearch {
 
         let amount = env::attached_deposit();
         let gas = env::prepaid_gas();
-		let promise_index = env::promise_create(account_id, &method_name, &arguments, amount, gas);
+		let promise_index = env::promise_create(account_id, &method_name.0, &arguments.0, amount, gas);
         env::promise_return(promise_index);
 	}
-
-    pub fn return_arguments(&self, arguments: Vec<u8>) {
-        env::log(format!("return_arguments: {:?}", arguments).as_bytes());
-    }
 }
